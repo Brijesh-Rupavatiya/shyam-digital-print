3076aacf5beeb9a55ca1c83ab6639e713b2e5221
Brijesh-Rupavatiya <brijeshrupavatiya118@gmail.com>
2026-05-03 16:39:09 +0530

    SDP-2 feat: implement Laravel Sanctum authentication with login, profile, and logout endpoints

--- backend/app/Http/Controllers/Api/AuthController.php
+++ backend/app/Http/Controllers/Api/AuthController.php
@@ -19,9 +19,32 @@ class AuthController extends Controller
         $user = User::where('email', $request->email)->first();
 
         if (!$user || !Hash::check($request->password, $user->password)) {
-            return response()->json(['message', 'Invalid credentials'], 401);
+            return response()->json([
+                'message' => 'Invalid credentials'
+            ], 401);
         }
 
-        return response()->json(['message' => 'login successfully', 'user' => $user]);
+        // TOKEN GENERATE
+        $token = $user->createToken('admin-token')->plainTextToken;
+
+        return response()->json([
+            'message' => 'Login successful',
+            'token' => $token,
+            'user' => $user
+        ]);
+    }
+
+    public function profile(Request $request)
+    {
+        return response()->json($request->user());
+    }
+
+    public function logout(Request $request)
+    {
+        $request->user()->currentAccessToken()->delete();
+
+        return response()->json([
+            'message' => 'Logged out'
+        ]);
     }
 }
